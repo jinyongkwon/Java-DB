f46d23a805b1b65b958a8be08903b423b10d5d1e
jaewon2336 <xldzjqpf1588@naver.com>
2022-02-21 16:23:15 +0900

    강사님 문제풀이(수정)

---
+++ src/site/metacoding/db/hospapp/ex05/ResponseDto.java
@@ -0,0 +1,55 @@
+
+package site.metacoding.db.hospapp.ex05;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+class Body {
+    private Items items;
+    private Integer numOfRows;
+    private Integer pageNo;
+    private Integer totalCount;
+}
+
+@Data
+class Header {
+    private String resultCode;
+    private String resultMsg;
+}
+
+@Data
+class Item {
+    private String addr;
+    private Integer mgtStaDd;
+    private String pcrPsblYn;
+    private String ratPsblYn;
+    private Integer recuClCd;
+    private String rprtWorpClicFndtTgtYn;
+    private String sgguCdNm;
+    private String sidoCdNm;
+    private String telno;
+    private Integer XPos;
+    private Double XPosWgs84;
+    private Integer YPos;
+    private Double YPosWgs84;
+    private String yadmNm;
+    private String ykihoEnc;
+}
+
+@Data
+class Items {
+    private List<Item> item;
+}
+
+@Data
+class Response {
+    private Header header;
+    private Body body;
+}
+
+@Data
+public class ResponseDto {
+    private Response response;
+}
